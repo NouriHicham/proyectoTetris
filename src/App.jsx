9a5788255d625aa3e73ec5f2ea97ed9178acffac
NouriHicham <nourichamidhicham@fpllefia.com>
2025-01-17 17:54:13 +0100

    Crea componente para la vista Inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,21 @@
 import { useState } from "react";
 import { compareAsc, format } from "date-fns";
 import "./App.css";
+import {TablaPartidas} from "./tabla.jsx";
 
-function Puntuacion(props){
-
-  const fechaFormateada = format(props.date, 'dd/MM/yyyy');
-
-  return (
-    <tr>
-      <td>{props.name}</td>
-      <td>{props.score}</td>
-      <td>{fechaFormateada}</td>
-    </tr>
-  );
-}
-
-function TablaPartidas(){
-
-  const [partidas, setPartidas] = useState([
-    {name:"Juan", score: 333, date: new Date(2014, 1, 11)},
-    {name:"Jose", score: 523, date: new Date(2012, 12, 11)},
-    {name:"Jorge", score: 135, date: new Date(2012, 12, 11)}
-  ]);
-
-  function ordenarArray(){
-    const arrayOrdenada = partidas.sort((partidaA, partidaB) => partidaB.score - partidaA.score)
-    setPartidas([...arrayOrdenada]);
-  }
-
-  function addPartida(props){
-    const name = props.name;
-    const score = props.score;
-    const date = new Date();
-
-    setPartidas(arrayAntiguo =>[...arrayAntiguo, {name: name, score: score, date: date}])
-  }
-
-  return (
+function Vista(){
+  return(
     <>
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">Nombre</th>
-            <th scope="col">Puntuación <button className="btn btn-secondary" onClick={ordenarArray}>↑</button></th>
-            <th scope="col">Fecha</th>
-          </tr>
-        </thead>
-        <tbody>
-        {partidas.map((partida, index)=>(
-          <Puntuacion key={index} name={partida.name} score={partida.score} date={partida.date}/>
-          ))}
-        </tbody>
-      </table>
-      <button className="btn btn-secondary" onClick={() => addPartida({ name: "Maria", score: 450})}>Añadir partida</button>
+      <div id="intro" className="text-center p-5">
+        <p>Tetris és un videojoc de tipus trencaclosques. Fou inventat per l'enginyer informàtic rus Aleksei Pàjitnov l'any 1984,[1] mentre treballava a l'Acadèmia de Ciències de Moscou.</p>
+        <h2>Instruccions:</h2>
+        <p>Pots moure les peces fent servir les fletxes d'esquerra i dreta</p>
+        <p>Amb la fletxa avall pots girar la peça</p>
+        <p>'<strong>Ñ</strong>' per canviar la peça actual per la peça que està a punt de sortir (que pots veure a la columna de la dreta)</p>
+        <p>Al final de la partida podràs desar la teva puntuació, i verue el ranking de jugadors</p>
+        <button className="btn btn-success fs-1 mt-5">JUGAR</button>
+				<hr/>
+      </div>
     </>
   );
 }
@@ -61,7 +24,8 @@ function App() {
 
   return (
     <>
-      <TablaPartidas></TablaPartidas>
+      <Vista/>
+      <TablaPartidas/>
     </>
   );
 }
---
+++ src/tabla.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { compareAsc, format } from "date-fns";
+
+function Puntuacion(props){
+
+   const fechaFormateada = format(props.date, 'dd/MM/yyyy');
+ 
+   return (
+     <tr>
+       <td>{props.name}</td>
+       <td>{props.score}</td>
+       <td>{fechaFormateada}</td>
+     </tr>
+   );
+ }
+ 
+export function TablaPartidas(){
+ 
+   const [partidas, setPartidas] = useState([
+     {name:"Juan", score: 333, date: new Date(2014, 1, 11)},
+     {name:"Jose", score: 523, date: new Date(2012, 12, 11)},
+     {name:"Jorge", score: 135, date: new Date(2012, 12, 11)}
+   ]);
+ 
+   function ordenarArray(){
+     const arrayOrdenada = partidas.sort((partidaA, partidaB) => partidaB.score - partidaA.score)
+     setPartidas([...arrayOrdenada]);
+   }
+ 
+   function addPartida(props){
+     const name = props.name;
+     const score = props.score;
+     const date = new Date();
+ 
+     setPartidas(arrayAntiguo =>[...arrayAntiguo, {name: name, score: score, date: date}])
+   }
+ 
+   return (
+     <>
+       <table className="table">
+         <thead>
+           <tr>
+             <th scope="col">Nombre</th>
+             <th scope="col">Puntuación <button className="btn btn-secondary" onClick={ordenarArray}>↑</button></th>
+             <th scope="col">Fecha</th>
+           </tr>
+         </thead>
+         <tbody>
+         {partidas.map((partida, index)=>(
+           <Puntuacion key={index} name={partida.name} score={partida.score} date={partida.date}/>
+           ))}
+         </tbody>
+       </table>
+       <button className="btn btn-secondary" onClick={() => addPartida({ name: "Maria", score: 450})}>Añadir partida</button>
+     </>
+   );
+ }
