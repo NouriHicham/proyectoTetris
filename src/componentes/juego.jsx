38214f956150cfdd9a9813b80f20b29c5dade2de
NouriHicham <nourichamidhicham@fpllefia.com>
2025-01-31 16:09:28 +0100

    Implementa controlTeclas() y eventos de teclado

--- src/componentes/juego.jsx
+++ src/componentes/juego.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import {Panel} from "./panel"
 import { Piezas } from "./pieza";
 import { nuevaPieza } from "../lib/nuevaPieza";
@@ -8,6 +8,7 @@ export function Juego(){
    const [arrayCasillas, setCasillas] = useState(modelos.matriz);
    const [piezaActual, setPiezaactual] = useState(null)
 
+   //añade pieza arriba
    function pintarPieza(){
       const pieza = nuevaPieza();
       pieza.columna = Math.floor(Math.random() * 9) + 1;
@@ -21,21 +22,61 @@ export function Juego(){
             const dibujaCelda = pieza.columna + j;
 
             //esto quitalo mas tarde
-            if(nuevoPanel[dibujaFila][dibujaCelda]==1){
-               console.log('colision');
-               return;
-            }else{
-               nuevoPanel[dibujaFila][dibujaCelda] = celda;
-               setCasillas(nuevoPanel);
-            }
+            // if(nuevoPanel[dibujaFila][dibujaCelda]==1){
+            //    console.log('colision');
+            //    return;
+            // }else{
+            //    nuevoPanel[dibujaFila][dibujaCelda] = celda;
+            //    setCasillas(nuevoPanel);
+            // }
             //final de lo que tienes que quitar
  
          })
       })
 
-      
    }
 
+   //detectar teclas
+   useEffect(() => {
+      function Teclas(event){
+         switch(event.key) {
+            case "ArrowRight":
+               moverDer();
+               break;
+            case "ArrowLeft":
+               moverIzq();
+               break;
+            case "ArrowDown":
+               bajar();
+               break;
+            case "ArrowUp":
+               girar();
+               break;
+            default:
+               break;
+         }
+      };
+
+      window.addEventListener("keydown", Teclas);
+      return () => window.removeEventListener("keydown", Teclas);
+      
+   }, []);
+
+   function moverDer() {
+      console.log("Mover a la derecha");
+   }
+
+   function moverIzq() {
+      console.log("Mover a la izquierda");
+   }
+
+   function bajar() {
+      console.log("Bajar");
+   }
+
+   function girar() {
+      console.log("Girar");
+   }
 
    return(
    <>
